0fe74acf6cd16349fb650337770cb0dd9921c0d6
Fernando Valdes <fernando@valdes.com.mx>
2016-01-07 16:42:21 -0600

    Task 1: - Views & Controllers - Working with Firebase

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var app = angular
+  .module('TaskNinjaApp', [
+    'ngAnimate',
+    'ngResource',    
+    'ngRoute',    
+    'firebase'
+  ])
+  .constant('FURL', 'https://task-ninjafer.firebaseio.com/')  
+  .config(function ($routeProvider) {
+    $routeProvider      
+      .when('/', {
+        templateUrl: 'views/main.html'        
+      })
+      .when('/post', {
+        templateUrl: 'views/post.html'
+        controller: 'TaskController'
+      })
+      .when('/edit', {
+        templateUrl: 'views/edit.html'
+      })
+      .when('/browse', {
+        templateUrl: 'views/browse.html'
+        controller: 'TaskController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
